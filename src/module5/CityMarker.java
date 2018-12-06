413bdecab994aae59aec56034655171b26d29e92
mmncoder <mamnrashid@gmail.com>
2018-12-06 20:58:01 +0300

    All files have been added to upload to Github

---
+++ src/module5/CityMarker.java
@@ -0,0 +1,79 @@
+package module5;
+
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+import processing.core.PConstants;
+import processing.core.PGraphics;
+
+/** Implements a visual marker for cities on an earthquake map
+ * 
+ * @author UC San Diego Intermediate Software Development MOOC team
+ * @author MD AL MAMUNUR RASHID (Mamun)
+ *
+ */
+public class CityMarker extends CommonMarker  {
+	
+	public static int TRI_SIZE = 5;  // The size of the triangle marker
+	
+	public CityMarker(Location location) {
+		super(location);
+	}
+	
+	
+	public CityMarker(Feature city) {
+		super(((PointFeature)city).getLocation(), city.getProperties());
+		// Cities have properties: "name" (city name), "country" (country name)
+		// and "population" (population, in millions)
+	}
+
+	
+	/**
+	 * Implementation of method to draw marker on the map.
+	 */
+	public void drawMarker(PGraphics pg, float x, float y) {
+		// Save previous drawing style
+		pg.pushStyle();
+		
+		// IMPLEMENT: drawing triangle for each city
+		pg.fill(150, 30, 30);
+		pg.triangle(x, y-TRI_SIZE, x-TRI_SIZE, y+TRI_SIZE, x+TRI_SIZE, y+TRI_SIZE);
+		
+		// Restore previous drawing style
+		pg.popStyle();
+	}
+	
+	/** Show the title of the city if this marker is selected */
+	public void showTitle(PGraphics pg, float x, float y)
+	{
+		//save previous style
+		pg.pushStyle(); 
+		String text = getCity()+ " " + getCountry()+ " " + getPopulation() +"m";
+		//print the properties
+		pg.fill(0, 0, 0);
+		pg.text(text, x, y);
+		
+		//store the previous style
+		pg.pushStyle();
+	}
+	
+	
+	
+	/* Local getters for some city properties.  
+	 */
+	public String getCity()
+	{
+		return getStringProperty("name");
+	}
+	
+	public String getCountry()
+	{
+		return getStringProperty("country");
+	}
+	
+	public float getPopulation()
+	{
+		return Float.parseFloat(getStringProperty("population"));
+	}
+}
